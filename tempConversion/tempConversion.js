2858397e26269f8a44596ae3a28e27eeb67017dc
Thomas Henley <t4.crow@gmail.com>
2021-06-03 01:07:13 -0400

    first set of JS problems

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  return parseFloat(((f - 32) * 5 / 9).toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  return parseFloat((c * 9 / 5 + 32).toFixed(1))
 };
 
 module.exports = {
